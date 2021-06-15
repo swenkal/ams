6f288010e873a9bf647cdff693e46c6c1fff89e9
Alex Podshivalov <ksndr.pdsh@gmail.com>
2021-06-15 19:44:04 +0300

    Added collecting data for download archive files

--- templates/test/processors/createZip.js
+++ templates/test/processors/createZip.js
@@ -1,6 +1,8 @@
 const qs = require('querystring'),
       archiver = require('archiver'),
       router = require('../../../router.js'),
+      cookie = require('cookie'),
+      http = require('http'),
       path = require('path');
 
 module.exports = {
@@ -12,11 +14,13 @@ module.exports = {
     if(request.method == 'POST'){
       router.downloadClientPostData(request, (err, data) => {
         if(err) return redirectTo400Page(response, callback);
+        let filesFromClient = [];
         try {
           const STORAGE_DATA_LOCATION = process.env['STORAGE_DATA_LOCATION'] ? `${process.env['STORAGE_DATA_LOCATION']}/private` : '';
           const PATH_TO_FILES = STORAGE_DATA_LOCATION || '/Users/ksndr/Projects/ams/data/private';
           let postData = qs.parse(data);
-          const filesFromClient = postData.files.map(curFile => {
+
+          filesFromClient = postData.files.map(curFile => {
             curFile = JSON.parse(curFile);
             curFile.fullPath = `${PATH_TO_FILES}${curFile.fullPath}`;
             return curFile;
@@ -40,6 +44,69 @@ module.exports = {
         } catch(err) {
           return redirectTo500Page(response,callback, err);
         }
+        let fileInfo = {};
+        db.collection('users').findOne(
+          { _id: sessionContext.id },
+          { group: 1, securityRole: 1 },
+          (err, user) => {
+            if (err) console.log(err);
+
+            let group = user.securityRole[0];
+            if (user.group) group = user.group;
+
+            db.collection('groups').findOne(
+              { _id: group },
+              { url: 1 },
+              (err, groupURL) => {
+                if (err) console.log(err);
+                if (groupURL) group = groupURL.url;
+
+                fileInfo.userGroup = group;
+
+                const fileIDs = filesFromClient.map(curFile => curFile.fullPath.split('/').pop());
+                console.log(fileIDs[0]);
+                db.collection('disciplines').findOne(
+                  { files: fileIDs[0] },
+                  { allias: 1 },
+                  (err, discipline) => {
+                    if (err) console.log(err);
+
+                    fileInfo.discipline = discipline.allias;
+                    fileInfo.downloadDate = Date.now();
+                    const rawCookies = request.headers.cookie;
+                    const cookies = cookie.parse(rawCookies);
+                    fileInfo.uniqueID = cookies['uniqueID'];
+                    fileInfo.visitID = cookies['visitID'];
+                    let [,,domain, ...refererUrl] = request.headers.referer.split('/');
+                    fileInfo.isArchive = true;
+                    fileInfo.referrer = 'Empty';
+                    if (request.headers.referer)
+                        fileInfo.referrer = request.headers.referer;
+                    let downloadedFiles = [];
+                    for (let file of fileIDs) {
+                      fileInfo.fileID = file;
+                      downloadedFiles.push(fileInfo);
+                    }
+                    const options = {
+                      hostname: 'localhost',
+                      port: 5000,
+                      path: '/files',
+                      method: 'POST',
+                    };
+
+                    const req = http.request(options, res => {
+                      console.log(`statusCode: ${res.statusCode}`);
+                    })
+
+                    req.on('error', error => {
+                      console.error(error)
+                    });
+
+                    req.write(JSON.stringify(downloadedFiles));
+                    req.end();
+                  });
+                });
+          });
       });
     } else {
       return redirectTo404Page(response,callback);
